d30c1a21e22d2a9808984a0c258d7b634b6696e1
mattn <mattn.jp@gmail.com>
2011-06-29 01:50:57 +0900

    add script to make default config.pl

---
+++ make-config.pl
@@ -0,0 +1,22 @@
+use strict;
+use warnings;
+
+use URI;
+use Web::Scraper;
+
+my $id = shift || '1050889';
+my $staff = scraper {
+    process '//ul[contains(concat(" ",@class," ")," forks ")]/li/a[1]',
+      'forks[]' => '@href';
+};
+
+my $res = $staff->scrape( URI->new("https://gist.github.com/$id") );
+my %config = ( git_root => "gist-$id", pull_remote => [] );
+for my $item ( @{ $res->{forks} } ) {
+    push @{ $config{pull_remote} }, "git://gist.github.com" . $item->path;
+}
+
+use Data::Dumper;
+$Data::Dumper::Terse  = 1;
+$Data::Dumper::Indent = 1;
+print Dumper( [%config] );
