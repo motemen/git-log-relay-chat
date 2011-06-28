85a1325d0a45b515d51599b94762742647890f16
mattn <mattn.jp@gmail.com>
2011-06-29 02:45:47 +0900

    oops

--- make-config.pl
+++ make-config.pl
@@ -4,13 +4,13 @@ use warnings;
 use URI;
 use Web::Scraper;
 
-my $id = shift || '1050889';
+my $id = shift or die "usage: $0 [your-forked-id]";
 my $staff = scraper {
     process '//ul[contains(concat(" ",@class," ")," forks ")]/li/a[1]',
       'forks[]' => '@href';
 };
 
-my $res = $staff->scrape( URI->new("https://gist.github.com/$id") );
+my $res = $staff->scrape( URI->new("https://gist.github.com/1050889") );
 my %config = ( git_root => "gist-$id", pull_remote => [] );
 for my $item ( @{ $res->{forks} } ) {
     push @{ $config{pull_remote} }, "git://gist.github.com" . $item->path;
